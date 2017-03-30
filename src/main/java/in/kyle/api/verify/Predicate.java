92b8ec41fc8ce5f6c2fa89606ca12bcca95dbce7
Kyle <kylemock@rocketmail.com>
2017-03-29 19:16:48 -0600

    Sanity checks

--- src/main/java/in/kyle/api/verify/Predicate.java
+++ src/main/java/in/kyle/api/verify/Predicate.java
@@ -12,38 +12,45 @@ public abstract class Predicate<T> {
     protected final T compare;
     
     public Result isNotNull() {
-        return result(compare != null, "Value is not null: {}", compare);
+        return result(compare != null, "Value is null: {}", compare);
     }
     
     public Result isNull() {
-        return result(compare == null, "Value is null: {}", compare);
+        return result(compare == null, "Value is not null: {}", compare);
     }
     
     public Result isEqual(T t) {
+        isNotNull();
         return result(compare.equals(t), "Value is equal {} equals {}", compare, t);
     }
     
     public Result isNotEqual(T t) {
+        isNotNull();
         return result(!compare.equals(t), "Value is not equal {} not equal {}", compare, t);
     }
     
     public Result isSame(T t) {
+        isNotNull();
         return result(compare == t, "Value is same {} == {}", compare, t);
     }
     
     public Result isNotSame(T t) {
+        isNotNull();
         return result(compare != t, "Value is different {} != {}", compare, t);
     }
     
     public Result isInstanceOf(Class<?> clazz) {
+        isNotNull();
         return result(clazz.isInstance(compare), "Value is instance of {}", clazz.getName());
     }
     
     public Result isNotInstanceOf(Class<?> clazz) {
+        isNotNull();
         return result(!clazz.isInstance(compare), "Value not instance of {}", clazz.getName());
     }
     
     public Result isExactType(Class<?> clazz) {
+        isNotNull();
         return result(clazz.equals(compare.getClass()),
                       "Value class not {}, but instead {}",
                       clazz.getName(),
@@ -51,6 +58,7 @@ public abstract class Predicate<T> {
     }
     
     public Result isNotExactType(Class<?> clazz) {
+        isNotNull();
         return result(!clazz.equals(compare.getClass()),
                       "Value is not class {}, but instead {}",
                       compare.getClass().getName());
